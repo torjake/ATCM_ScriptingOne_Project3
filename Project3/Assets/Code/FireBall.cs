3800e856cbd271dcfaf7cd53a713adda768ae555
torjake <jacobhan6@gmail.com>
2021-05-04 09:46:42 -0500

    the game

---
+++ Project3/Assets/Code/FireBall.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FireBall : MonoBehaviour
+{
+    float Speed;
+    public Rigidbody2D rb;
+    public GameObject spawnPoint;
+    //
+    public GameObject Player_Ref;
+    //
+    public Animator animator;
+    //
+    public AudioSource pain;
+    public AudioSource parry;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = Random.Range(5, 15);
+        print(Speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(rb.position + Vector2.left * Speed * Time.fixedDeltaTime);
+        //
+        if (rb.transform.position.x < -14f)
+        {
+            rb.position = spawnPoint.transform.position;
+            Speed = Random.Range(5, 15);
+            animator.SetBool("Damage_Anim", false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            if (Player_Ref.GetComponent<Player_Parry>().isParrying == true)
+            {
+                print("Parry");
+                rb.position = spawnPoint.transform.position;
+                animator.SetBool("Parry_Anim", true);
+                //
+                parry.enabled = false;
+                parry.enabled = true;
+            }
+            if (Player_Ref.GetComponent<Player_Parry>().isParrying != true)
+            {
+                print("hit");
+                animator.SetBool("Damage_Anim", true);
+                //
+                pain.enabled = false;
+                pain.enabled = true;
+            }
+        }
+    }
+}
---
+++ Project3/Assets/Code/Player_Parry.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Parry : MonoBehaviour
+{
+    float timer;
+    public float ParryTimeLength;
+    public bool isParrying = false;
+    //
+    public Animator animator;
+    //
+    public GameObject parryEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = ParryTimeLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && timer == ParryTimeLength)
+        {
+            isParrying = true;   
+        }
+        if (isParrying == true)
+        {
+            timer -= Time.fixedDeltaTime;
+            if (timer <= 0)
+            {
+                isParrying = false;
+                timer = ParryTimeLength;
+                animator.SetBool("Parry_Anim", false);
+            }
+        }
+        //
+        if (animator.GetBool("Parry_Anim") == true)
+        {
+            parryEffect.SetActive(true);
+        }
+        if (animator.GetBool("Parry_Anim") == false)
+        {
+            parryEffect.SetActive(false);
+        }
+        //
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
